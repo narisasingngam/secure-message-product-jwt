4116f9ed3d8a7f10afa4046a167fb36b874cb71e
Mint <narisa.si@ku.th>
2021-03-11 11:58:14 +0700

    add project

---
+++ src/main/java/ku/message/model/Product.java
@@ -0,0 +1,36 @@
+package ku.message.model;
+
+public class Product {
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    private long id;
+
+    private String name;
+    private double price;
+
+    // . . . generate all getters / setters ...
+}
+
